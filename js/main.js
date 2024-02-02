77446aaa44ff3d7f1a09d3bc9c7789a748fe05cc
nimer21 <nimerel_sayed@yahoo.com>
2024-02-02 22:05:16 +0200

    4th commit

--- js/main.js
+++ js/main.js
@@ -6,16 +6,25 @@ registerForm.onsubmit = function (e) {
   e.preventDefault();
   var elements = e.target.elements;
   var amount = elements[0].value;
-  console.log(amount);
+  console.log("amount= "+amount);
 
   var e = document.getElementById("exchange");
   var value = e.value;
   var text = e.options[e.selectedIndex].text;
-  console.log(text);
-  if (text == "dinar") result = amount * 0.19;
-  else if (text == "dollar") result = amount * 0.27;
-  else result = amount;
-  document.querySelector(".result").innerHTML = result.toFixed(2);
+  console.log("Currency= "+text);
+  if (text == "dinar"){
+    result = amount * 0.19;
+    document.querySelector(".result").innerHTML = Number(result).toFixed(2)+" JOD";
+  } 
+  else if (text == "dollar") {
+    result = amount * 0.27;
+    document.querySelector(".result").innerHTML = Number(result).toFixed(2)+" USD"; 
+  }
+  else {
+    result = amount;
+    document.querySelector(".result").innerHTML = Number(result).toFixed(2)+" NIS"; 
+  }
+  
 };
 
 
